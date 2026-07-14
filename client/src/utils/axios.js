2dd87a753a12c7fe03c3c770e018bef345f81870
Yogesh Sadgir <yogeshsadgir05@gmail.com>
2026-07-14 11:38:21 +0530

    Fix production BASE_URL and LawOfTheDay loader bugs

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL="http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 const axiosInstance = axios.create({
     baseURL : `${BASE_URL}/api`,
